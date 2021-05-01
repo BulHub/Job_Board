4838631e534373ba14bbddcc5a580f9732200f1e
Bulat <programming.languages.only@gmail.com>
2021-05-01 15:50:06 +0300

    Delete UserValidator.java

---
+++ Job_Board/src/main/java/com/bulat/jobboard/service/impl/EmailServiceImpl.java
@@ -0,0 +1,14 @@
+package com.bulat.jobboard.service.impl;
+
+import com.bulat.jobboard.model.User;
+import com.bulat.jobboard.service.EmailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void send(User user) {
+
+    }
+}
